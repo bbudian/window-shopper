bbbe6a32340f4b69f526e5d3fa9de4462d26e6c7
Ben <bbudian0@gmail.com>
2017-08-09 15:26:00 -0400

    Added logic for calculation

--- window-shopper/Controller/MainViewController.swift
+++ window-shopper/Controller/MainViewController.swift
@@ -12,7 +12,9 @@ class MainViewController: UIViewController {
 
     @IBOutlet weak var wageTextField: CurrencyTextField!
     @IBOutlet weak var priceTextField: CurrencyTextField!
-    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var hoursLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,13 +23,32 @@ class MainViewController: UIViewController {
         calcButton.setTitle("Calculate", for: .normal)
         calcButton.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
         calcButton.addTarget(self, action: #selector(MainViewController.calculate), for: .touchUpInside)
+        
         wageTextField.inputAccessoryView = calcButton
         priceTextField.inputAccessoryView = calcButton
         
+        hideShowLabels(isHidden: true)
+    }
+    
+    func hideShowLabels(isHidden: Bool){
+        resultLabel.isHidden = isHidden
+        hoursLabel.isHidden = isHidden
     }
     
     @objc func calculate(){
-        print("We got here")
+        if let wageText = wageTextField.text, let priceText = priceTextField.text{
+            if let wage = Double(wageText), let price = Double(priceText){
+                view.endEditing(true)
+                hideShowLabels(isHidden: false)
+                resultLabel.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
+    }
+    
+    @IBAction func clearCalculatorPressed(_ sender: Any) {
+        hideShowLabels(isHidden: true)
+        wageTextField.text = ""
+        priceTextField.text = ""
     }
 }
 
