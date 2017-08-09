b403696df049362734dd0c391bcc42d928adbcd3
Ben <bbudian0@gmail.com>
2017-08-09 14:35:14 -0400

    Setup input and showing of the keyboard and calculate button using the .inputAccessoryView

--- window-shopper/Controller/MainViewController.swift
+++ window-shopper/Controller/MainViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var wageTextField: CurrencyTextField!
+    @IBOutlet weak var priceTextField: CurrencyTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcButton.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        calcButton.setTitle("Calculate", for: .normal)
+        calcButton.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+        calcButton.addTarget(self, action: #selector(MainViewController.calculate), for: .touchUpInside)
+        wageTextField.inputAccessoryView = calcButton
+        priceTextField.inputAccessoryView = calcButton
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func calculate(){
+        print("We got here")
     }
-
-
 }
 
